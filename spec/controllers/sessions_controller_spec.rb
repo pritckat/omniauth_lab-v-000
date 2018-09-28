ca3ede0794ff592b2eb36dfdefe6e1534e517df1
Katrina Pritchard <pritckat@gmail.com>
2018-09-28 21:15:25 +0000

    automatically backed up by learn

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,14 +1,14 @@
 # require 'rails_helper'
-# 
+#
 # RSpec.describe SessionsController, type: :controller do
-# 
+#
 #   before(:each) do
 #     User.destroy_all
 #   end
-# 
+#
 #   let(:user) {User.create(:name => "Blake Johnson", :uid => 1234567
 #     )}
-# 
+#
 #   describe 'get create' do
 #     it 'finds user if it exists and logs the user in' do
 #       auth = {
@@ -24,7 +24,7 @@
 #       get :create
 #       expect(@request.session[:user_id]).to eq(user.id)
 #     end
-# 
+#
 #     it 'creates user if it doesnt exist in the db' do
 #       auth = {
 #         :provider => 'facebook',
@@ -39,7 +39,7 @@
 #       get :create
 #       expect(@request.session[:user_id]).to eq(1)
 #     end
-# 
+#
 #     it "create the user correctly" do
 #       name = 'Blake Johnson'
 #       auth = {
